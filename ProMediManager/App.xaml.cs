b1778cdaf87a6e95b0440af4b650ffbb66355e85
CC-Student <azlanainto97@gmail.com>
2025-07-15 22:54:44 +0200

    Projektdateien hinzufügen.

---
+++ ProMediManager/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ProMediManager
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ProMediManager/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace ProMediManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
